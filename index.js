926365748abd8e85dcde7e11ebf9c0a430bee1f3
Zach <zawood11@gmail.com>
2021-09-30 00:53:34 -0400

    commit tree 07028f34505d8f5eddf65a66410de71ce6259772

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
 function switchOnCharmFromTip(){
